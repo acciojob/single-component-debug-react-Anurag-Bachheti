5898c88b65d8b7dc89f5888c0f81e020082dee51
HERCULES0000 <anuragbachheti1999@gmail.com>
2025-05-11 16:26:17 +0530

    Completed rendering React component on DOM node

--- parser.js
+++ parser.js
@@ -6,4 +6,6 @@ const options = {compact: true, ignoreComment: true, spaces: 4};
 
 const result = parser.xml2json(xml, options)
 
+const xmlData = fs.readFileSync('./src/data/junit.xml', 'utf-8');
+
 console.log(result);
